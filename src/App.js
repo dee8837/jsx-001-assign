316b149506070366da800e6410ba06e813f7cdc8
Deepak Shahi <96306408+dee8837@users.noreply.github.com>
2022-11-04 19:32:18 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,31 @@
+
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>About Us</h1>
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum indus.
+      </p>
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unIt was popularised in the 1960s with the
+        release of Letraset sheets containing Lorem Ipsum passages, and more
+        recently with desktop publishing software like Aldus PageMaker including
+        versions of Lorem Ipsum.
+      </p>
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an un
+      </p>
+
+    </div>
+  );
+}
+
+export default App;
